af06bb22cf6aec4269f117d43c9c935c297adf2f
Nathan Joel Nakatsuka <nathannakatsuka@MacBook-Pro-201.local>
2020-02-04 13:55:50 -0500

    Change

--- ContamLD_analyses/helperdir/makejinfiles_ExtCorr_New.R
+++ ContamLD_analyses/helperdir/makejinfiles_ExtCorr_New.R
@@ -8,18 +8,18 @@ for(j in 1:nrow(files)){
 args1=files[j,1]
 args3=files[j,2]
 setwd(paste(args[1],"/directories/",args1,"_jackknife",sep=""))
-jinfile=read.table(paste(args1,"_fullreadpairs.jin",sep=""),header=F)
+jinfile=read.table(paste(args1,"_",args3,"_",args[3],"panel_fullreadpairs.jin",sep=""),header=F)
 ### Loop through chromosomes; get values
 for(i in 1:22){
-score=read.table(paste(args1,"_",args3,"panel_1ind_noChr",i,"_topscore_fullreads.txt",sep=""),header=F)
+score=read.table(paste(args1,"_",args3,"_",args[3],"panel_1ind_noChr",i,"_topscore_fullreads.txt",sep=""),header=F)
 jinfile[i,3]=score[1,1]
 }
 num=mean(jinfile[,3])
-writejin <- file(paste(args1,"_",args3,"panel_scores_reads_ExtCorr.jin",sep=""),"w")
+writejin <- file(paste(args1,"_",args3,"_",args[3],"panel_scores_reads_ExtCorr.jin",sep=""),"w")
 write.table(jinfile,writejin,sep="\t",quote=FALSE,col.names=FALSE,row.names=FALSE)
 flush(writejin)
 close(writejin)
-writenum <- file(paste(args1,"_",args3,"panel_scores_reads_ExtCorr_meanscore.txt",sep=""),"w")
+writenum <- file(paste(args1,"_",args3,"_",args[3],"panel_scores_reads_ExtCorr_meanscore.txt",sep=""),"w")
 write.table(num,writenum,sep="\t",quote=FALSE,col.names=FALSE,row.names=FALSE)
 flush(writenum)
 close(writenum)
