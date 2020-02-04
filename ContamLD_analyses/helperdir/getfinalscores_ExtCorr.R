4c4dd7b78d63f315fd12ace63d1d72fc87efab95
Nathan Joel Nakatsuka <nathannakatsuka@MacBook-Pro-201.local>
2020-02-04 13:54:57 -0500

    Change

--- ContamLD_analyses/helperdir/getfinalscores_ExtCorr.R
+++ ContamLD_analyses/helperdir/getfinalscores_ExtCorr.R
@@ -8,11 +8,12 @@ for(k in 1:nrow(files)){
 args1=as.character(files[k,1])
 args3=as.character(files[k,2])
 setwd(paste(args[1],"/directories/",args1,"_jackknife",sep=""))
-score=read.table(paste(args1,"_",args3,"panel_scores_reads_ExtCorr.jout",sep=""),header=F)
+score=read.table(paste(args1,"_",args3,"_",args[4],"panel_scores_reads_ExtCorr.jout",sep=""),header=F)
 Table[k,1]=args1
 Table[k,2]=score[1,1]-as.numeric(args[3])
 Table[k,3]=score[1,2]
 Table[k,4]=args3
+Table[k,5]=args[4]
 }
-colnames(Table) <- c("Sample_ID","ContamLD_ExtCorr_Estimate","ContamLD_ExtCorr_Estimate_se","Panel")
-write.table(Table,file=paste(args[1],"/FinalContamScores_ExtCorr_",args[2],sep=""),sep="\t",quote=FALSE,col.names=TRUE,row.names=FALSE)
+colnames(Table) <- c("Sample_ID","ContamLD_ExtCorr_Estimate","ContamLD_ExtCorr_Estimate_se","Panel","Panel_Type")
+write.table(Table,file=paste(args[1],"/FinalContamScores_ExtCorr_",args[4],"_",args[2],sep=""),sep="\t",quote=FALSE,col.names=TRUE,row.names=FALSE)
