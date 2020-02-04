2a3a121054061f6e8782d74e195af649abefc880
Nathan Joel Nakatsuka <nathannakatsuka@MacBook-Pro-201.local>
2020-02-04 13:55:09 -0500

    Change

--- ContamLD_analyses/helperdir/getjackknifeses_DamCorr.sh
+++ ContamLD_analyses/helperdir/getjackknifeses_DamCorr.sh
@@ -3,10 +3,10 @@
 while read sampleID panel; do
 cd $2/directories/${sampleID}_jackknife
 
-var=$(<${sampleID}_${panel}panel_scores_reads_DamCorr_meanscore2.txt)
-$1/helperdir/dowtjack -i ${sampleID}_${panel}panel_scores_reads_DamCorr.jin -m ${var} -o ${sampleID}_${panel}panel_scores_reads_DamCorr.jout
+var=$(<${sampleID}_${panel}_$4panel_scores_reads_DamCorr_meanscore2.txt)
+$1/helperdir/dowtjack -i ${sampleID}_${panel}_$4panel_scores_reads_DamCorr.jin -m ${var} -o ${sampleID}_${panel}_$4panel_scores_reads_DamCorr.jout
 
-var2=$(<${sampleID}_${panel}panel_kscores_reads_DamCorr_meanscore2.txt)
-$1/helperdir/dowtjack -i ${sampleID}_${panel}panel_kscores_reads_DamCorr.jin -m ${var2} -o ${sampleID}_${panel}panel_kscores_reads_DamCorr.jout
+var2=$(<${sampleID}_${panel}_$4panel_kscores_reads_DamCorr_meanscore2.txt)
+$1/helperdir/dowtjack -i ${sampleID}_${panel}_$4panel_kscores_reads_DamCorr.jin -m ${var2} -o ${sampleID}_${panel}_$4panel_kscores_reads_DamCorr.jout
 
 done < $2/$3
