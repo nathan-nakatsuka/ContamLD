a681c821fee89933a6a5ee4c0c979e0ba9613bd6
Nathan Joel Nakatsuka <nathannakatsuka@MacBook-Pro-201.local>
2020-02-04 13:49:54 -0500

    Change

--- ContamLD_analyses/helperdir/multiple_processes_contamination_R.py
+++ ContamLD_analyses/helperdir/multiple_processes_contamination_R.py
@@ -3,16 +3,17 @@ from random import randint
 from subprocess import check_output
 import sys
 
-def sample_function(rscript_filename, directory1, directory2, name, pop):
-	check_output(["Rscript",rscript_filename, directory1, directory2, name, pop])
+def sample_function(rscript_filename, directory1, directory2, name, pop, size):
+	check_output(["Rscript",rscript_filename, directory1, directory2, name, pop, size])
 
 jobs = ['./helperdir/ContamLD_main_DD.R','./helperdir/ContamLD_main_DU.R','./helperdir/ContamLD_main_UU.R','./helperdir/ContamLD_main_full.R']
 directory1=sys.argv[1]
 directory2=sys.argv[2]
 name=sys.argv[3]
 pop=sys.argv[4]
+size=sys.argv[5]
 
-processes = [Process(target=sample_function, args=([job,directory1,directory2,name,pop])) for job in jobs]
+processes = [Process(target=sample_function, args=([job,directory1,directory2,name,pop,size])) for job in jobs]
 
 for p in processes:
 	p.start()
